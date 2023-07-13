4e99865bbe9bd48a184cbb2623154b132a208983
Ane Morkemo <ane.kirstine@gmail.com>
2023-07-13 12:19:10 +0200

    part1

---
+++ day_01/main.py
@@ -0,0 +1,18 @@
+def part_one(file: str) -> int:
+    elves_cals = process_file(file)
+    # print(elves_cals)
+    cal_sums = [ sum(elf_cals) for elf_cals in elves_cals ]
+    return max(cal_sums)
+
+def process_file(file: str) -> list[list[int]]:
+    with open(file, 'r') as f:
+        elves = f.read().split("\n\n")
+        return [list(map(int, elf.split())) for elf in elves]
+
+def part_two(file: str) -> 
+if __name__ == "__main__":
+    input_path = "./day_01/ex.txt"
+    print("==== PART 1 ====")
+    print(part_one(input_path))
+    print("==== PART 2 ====")
+    print(part_one(input_path))
